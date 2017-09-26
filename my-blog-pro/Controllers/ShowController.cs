ccc5fee056bdae3596463da9882cab0aa6152591
923035434 <923035434@qq.com>
2017-09-26 12:55:10 +0800

    完成博客展示页面

---
+++ my-blog-pro/Controllers/ShowController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace my_blog_pro.Controllers
+{
+    public class ShowController : Controller
+    {
+        // GET: Show
+        public ActionResult Index()
+        {
+            return Redirect("/Show/M");
+            return View();
+        }
+
+        public ActionResult M()
+        {
+            return View();
+        }
+    }
+}
